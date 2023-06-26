a4d1d6e81f9d7338ec37ed8ad9decfd6234acc5b
yongfratama <127808914+yongfratama@users.noreply.github.com>
2023-06-26 19:47:16 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Uas Webpro Nim</title>
+        <meta name="description" content="ELSA Elearning Instiki pada bagian pertemuan 16 Ujian Akhir">
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" type="image/png" href="assets/img/favicon.png">
+
+        <!-- Page Style -->
+        <link rel="stylesheet" type="text/css" href="assets/styles.css?version=1">
+    </head>
+
+    <body>
+        <!-- Header with Image & Text -->
+        <div class="hero-image" style="background-image: url('assets/img/header-bg.jpg');">
+            <div class="coverDiv">
+                <img class="coverArt rounded" src="assets/img/image-logo.png" alt="Logo" height="350" width="350"></img>
+                <div class="hero-text">
+                    <h1>Uas Webpro Nim</h1>
+                    <h3>ELSA Elearning Instiki pada bagian pertemuan 16 Ujian Akhir</h3>
+                </div>
+            </div>
+        </div>
+
+        <!-- Description Section -->
+        <div class="descriptionDiv">
+
+            <!-- Section -->
+            <h2>Project Grid</h2>
+
+            <!-- Portfolio Grid -->
+            <div class="grid">
+
+                <!-- Portfolio Item -->
+                <a href="image-example.html" class="card">
+                    <div class="content">
+                        <img src="assets/img/card1.jpg" alt="Card Image" height="390" width="390"></img>
+                        <div class="card-body">
+                            <div class="subtitle">CATEGORY</div>
+                            <div class="title">Card Title</div>
+                            <div class="description">Short Description</div>
+                        </div>
+                    </div>
+                </a>
+
+                <!-- Portfolio Item -->
+                <a href="image-example.html" class="card">
+                    <div class="content">
+                        <img src="assets/img/card2.jpg" alt="Card Image" height="390" width="390"></img>
+                        <div class="card-body">
+                            <div class="subtitle">CATEGORY</div>
+                            <div class="title">Card Title</div>
+                            <div class="description">Short Description</div>
+                        </div>
+                    </div>
+                </a>
+
+                <!-- Portfolio Item -->
+                <a href="image-example.html" class="card">
+                    <div class="content">
+                        <img src="assets/img/card3.jpg" alt="Card Image" height="390" width="390"></img>
+                        <div class="card-body">
+                            <div class="subtitle">CATEGORY</div>
+                            <div class="title">Card Title</div>
+                            <div class="description">Short Description</div>
+                        </div>
+                    </div>
+                </a>
+
+                <!-- Portfolio Item -->
+                <a href="image-example.html" class="card">
+                    <div class="content">
+                        <img src="assets/img/card4.jpg" alt="Card Image" height="390" width="390"></img>
+                        <div class="card-body">
+                            <div class="subtitle">CATEGORY</div>
+                            <div class="title">Card Title</div>
+                            <div class="description">Short Description</div>
+                        </div>
+                    </div>
+                </a>
+
+                <!-- Portfolio Item -->
+                <a href="image-example.html" class="card">
+                    <div class="content">
+                        <img src="assets/img/card5.jpg" alt="Card Image" height="390" width="390"></img>
+                        <div class="card-body">
+                            <div class="subtitle">CATEGORY</div>
+                            <div class="title">Card Title</div>
+                            <div class="description">Short Description</div>
+                        </div>
+                    </div>
+                </a>
+
+                <!-- Portfolio Item -->
+                <a href="image-example.html" class="card">
+                    <div class="content">
+                        <img src="assets/img/card6.jpg" alt="Card Image" height="390" width="390"></img>
+                        <div class="card-body">
+                            <div class="subtitle">CATEGORY</div>
+                            <div class="title">Card Title</div>
+                            <div class="description">Short Description</div>
+                        </div>
+                    </div>
+                </a>
+
+                <!-- Portfolio Item -->
+                <a href="image-example.html" class="card">
+                    <div class="content">
+                        <img src="assets/img/card7.jpg" alt="Card Image" height="390" width="390"></img>
+                        <div class="card-body">
+                            <div class="subtitle">CATEGORY</div>
+                            <div class="title">Card Title</div>
+                            <div class="description">Short Description</div>
+                        </div>
+                    </div>
+                </a>
+
+                <!-- Portfolio Item -->
+                <a href="image-example.html" class="card">
+                    <div class="content">
+                        <img src="assets/img/card8.jpg" alt="Card Image" height="390" width="390"></img>
+                        <div class="card-body">
+                            <div class="subtitle">CATEGORY</div>
+                            <div class="title">Card Title</div>
+                            <div class="description">Short Description</div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <!-- Section -->
+            <h2>About This Project</h2>
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+        </div>
+
+        <!-- Feel free to put your own text here or share the love. -->
+        <div class="footer">
+            <p class="subtle"><i>example.com</i></p>
+        </div>
+
+        <!-- Navigation Script -->
+        <script>
+            function myFunction() {
+                var x = document.getElementById("myTopnav");
+                if (x.className === "topnav") {
+                    x.className += " responsive";
+                } else {
+                    x.className = "topnav";
+                }
+            }
+        </script>
+    </body>
+</html>
